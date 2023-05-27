91230b49ee0af4cfee6a5f59a65012e80a1f2831
gharieb khalifa <blacksnow.soon@gmail.com>
2023-05-27 12:52:17 +0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  rules: {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    duobleQuote: true,
+    trailingComma: 'all',
+    skipBlankLines: true,
+    ignoreComments: true,
+    requirePragma: true,
+  }
+};
---
+++ AppStyles.js
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+'use strict';
+import {StyleSheet} from 'react-native';
+
+const AppStyles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    backgroundColor: 'blue',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default AppStyles;
